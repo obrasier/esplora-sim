6b83ff89ca48d2472a7628622710fb421825c20b
Owen Brasier <owen.brasier@sydney.edu.au>
2017-03-30 10:16:26 +1100

    moved inc folder

--- inc/ultoa.h
+++
@@ -1,9 +0,0 @@
-#ifndef ULTOA_H_
-#define ULTOA_H_
-
-char* ultoa( unsigned long __val, char* __s, int __radix );
-char* ltoa( long __val, char* __s, int __radix );
-char* itoa( int __val, char* __s, int __radix );
-char* utoa( unsigned int __val, char* __s, int __radix );
-
-#endif
