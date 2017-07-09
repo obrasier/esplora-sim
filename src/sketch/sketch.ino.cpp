056dac0619e5c38c7175e0809dccefa56130f126
Owen Brasier <owen.brasier@sydney.edu.au>
2017-07-10 09:22:03 +1000

    Added rounding to set_pwm_period for the tone fucntion

--- src/sketch/sketch.ino.cpp
+++ src/sketch/sketch.ino.cpp
@@ -1,8 +1,6 @@
 #include <Esplora.h>
 void setup() {}
 void loop() {
-  Esplora.writeRed(255);
-  delay(1000);
-  Esplora.writeRed(200);
-  delay(1000);
+    Esplora.tone(1024);
+    delay(1000);
 }
